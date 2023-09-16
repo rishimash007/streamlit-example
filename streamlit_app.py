640b79e32116f6d221d8bfb0164f07c3baad1423
Rishi Maheshwari <rishi@Rishis-MacBook-Air.local>
2023-09-16 00:31:57 -0400

    updated for tracker gsheet

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,77 @@
-from collections import namedtuple
-import altair as alt
-import math
+from google.oauth2 import service_account
+from gspread_pandas import Spread,Client
 import pandas as pd
 import streamlit as st
+from datetime import datetime, timedelta
 
-"""
-# Welcome to Streamlit!
+# Create a Google Authentication connection object
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "Option Tracker"
+spread = Spread(spreadsheetname,client = client)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+sh = client.open(spreadsheetname)
+#worksheet_list = sh.worksheets()
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Get the sheet as dataframe
+def load_the_spreadsheet(spreadsheetname):
+    worksheet = sh.worksheet(spreadsheetname)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+def convert_to_date(date_integer):
+    year = 2000 + date_integer // 10000     # Convert "15" to 2015
+    month = (date_integer // 100) % 100  # Extract the month, which is "08"
+    day = (date_integer % 100) # Extract the day, which is "23"
 
-    points_per_turn = total_points / num_turns
+    # Create a datetime object
+    date_datetime = datetime(year, month, day)
+    return date_datetime
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+def clean(df):
+    df["Expirary"] = df["Expirary"].apply(lambda x : convert_to_date(x))
+    df["Sold"] = df["Sold"].apply(lambda x : pd.to_datetime(x))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    for col in ["Premium","Strike","Total Credit","Cost Avg"]:
+        df[col] = df[col].apply(lambda x : float(x.replace("$", "")))
+
+    for col in ["Annualized Return"]:
+        df[col] = df[col].apply(lambda x : float(x.replace("%", "")))
+    return df
+
+st.header(':chart_with_upwards_trend: Cloner Portfolio Tracker')
+
+# Check whether the sheets exists
+# what_sheets = worksheet_names()
+# #st.sidebar.write(what_sheets)
+# ws_choice = st.sidebar.radio('Available worksheets',what_sheets)
+
+# Load data from worksheets
+df = load_the_spreadsheet("main")
+
+df = clean(df)
+
+df = df[df["Consideration"]=='A']
+
+realized_income = df[(df["Exercised?"]=="FALSE")&(df["Days Away"]<=0)]
+income = realized_income["Total Credit"].sum()
+avg_pct_return = realized_income["Total Credit"].sum()/realized_income["Cost Avg"].sum()/100
+
+realized_income["f_tot"] = realized_income["Cost Avg"]/realized_income["Cost Avg"].sum()
+realized_income["f_tot_ret"] = realized_income["Annualized Return"]*realized_income["f_tot"]
+
+avg_annualized_pct_return = realized_income["f_tot_ret"].sum()/100
+
+
+col0, col1, col2= st.columns(3)
+col0.metric("Days Since Inception", (realized_income["Expirary"].max().date() - realized_income["Sold"].min().date()).days ,365)
+col1.metric("Realized Total Income", "${:.1f}".format(income), "$1000")
+col2.metric("Realized Avg. Return (ITD)", "{:.2%}".format(avg_pct_return), "{:.2%} (Annualized)".format(avg_annualized_pct_return))
+
+
+st.table(df)
